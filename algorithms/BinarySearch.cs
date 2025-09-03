8a25eca465f97e43c0ffcf093126baf088d4a047
yosefgue <yosefgue6@gmail.com>
2025-09-04 00:45:59 +0100

    binary search

---
+++ algorithms/BinarySearch.cs
@@ -0,0 +1,31 @@
+﻿namespace MyApp
+{
+    public static class BinarySearch
+    {
+        public static int? FindTarget(List<int> numbers, int target)
+        {
+            int low = 0;
+            int high = numbers.Count - 1;
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+                int guess = numbers[mid];
+                if (guess == target)
+                {
+                    return numbers[mid];
+                }
+                else if (guess > target)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            return null;
+        }
+
+    }
+
+}
---
+++ unitTests/BinarySearchTest.cs
@@ -0,0 +1,19 @@
+using MyApp;
+namespace UnitTests
+{
+    public class BinarySearchTest
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, 4, 4)]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, 3, 3)]
+        [InlineData(new int[] { 1, 2, 3, 4, 5, 9, 10, 11 }, 16, null)]
+        [InlineData(new int[] { 10, 19, 32, 34, 55, 65, 67, 71, 76, 80, 86, 93, 99, 129 }, 71, 71)]
+        [InlineData(new int[] { 1, 5, 9, 100, 203, 250, 299, 310 }, 311, null)]
+        public void Test(int[] ListValue, int target, int? expected)
+        {
+            var list = ListValue.ToList();
+            var result = BinarySearch.FindTarget(list, target);
+            Assert.Equal(expected, result);
+        }
+    }
+}
