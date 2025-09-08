ff8ef3de753299ecff696ac4b41a5c79b61cb4e3
yosefgue <yosefgue6@gmail.com>
2025-09-08 03:37:17 +0100

    selection sort algorithm

--- algorithms/BinarySearch.cs
+++ algorithms/BinarySearch.cs
@@ -1,4 +1,10 @@
-﻿namespace MyApp
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithms
 {
     public static class BinarySearch
     {
---
+++ algorithms/SelectionSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithms
+{
+    public static class SelectionSort
+    {
+        public static int Smallest(List<int> anarray)
+        {
+            int smallestint = anarray[0];
+            int smallestintindex = 0;
+            for (int i = 1; i < anarray.Count; i++ )
+            {
+                if (smallestint > anarray[i])
+                {
+                    smallestint = anarray[i];
+                    smallestintindex = i;
+                }
+            }
+            return smallestintindex;
+        }
+        public static int[] Sort(int[] anarray)
+        {
+            List<int> copyarray = anarray.ToList();
+            List<int> list = new List<int> ();
+            for (int i = 0;  i < anarray.Length; i++ )
+            {
+                int smallestint = copyarray[Smallest(copyarray)];
+                list.Add(smallestint);
+                copyarray.Remove(smallestint);
+            }
+            return list.ToArray();
+        }
+    }
+}
