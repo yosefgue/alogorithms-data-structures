4796e8fc9745224ba5478f4e452fdef832218e5a
yosefgue <yosefgue6@gmail.com>
2025-09-09 20:16:05 +0100

    Simple recursive algorithm

---
+++ algorithms/Recursive.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithms
+{
+    public static class Recursive
+    {
+        public static int Factorial(int x)
+        {
+            if (x == 0) return 1;
+            return x * Factorial(x - 1); 
+        }
+    }
+}
+
+/* 
+The base case prevents the function from running infinitely.
+When it’s reached, it returns 1 back to the top of the call stack.
+Each waiting function then takes that value, uses it in its own calculation, and returns the result upward until the original call finishes.
+this is a simpe case to understand how things work.
+ */
---
+++ unitTests/RecursiveTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using algorithms;
+
+namespace UnitTests
+{
+    public class RecursiveTest
+    {
+        [Theory]
+        [InlineData(5, 120)]
+        [InlineData(10, 3628800)]
+        [InlineData(8, 40320)]
+        [InlineData(6, 720)]
+        [InlineData(12, 479001600)]
+        [InlineData(4, 24)]
+        [InlineData(3, 6)]
+        [InlineData(1, 1)]
+        [InlineData(7, 5040)]
+        public void Test(int value, int expected)
+        {
+            var result = Recursive.Factorial(value);
+            Assert.Equal(expected, result);
+        }
+    }
+}
