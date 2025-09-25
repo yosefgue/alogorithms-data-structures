52b6718436b5a5cfbaf90aa6f48ad9303028613f
yosefgue <yosefgue6@gmail.com>
2025-09-25 03:16:40 +0100

    added more types of the recursive algorithm

---
+++ algorithms/RecursiveMax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithms
+{
+    public static class RecursiveMax
+    {
+        public static int Max(List<int> nums)
+        {
+            if (nums.Count == 2)
+            {
+                return nums[0] > nums[1] ? nums[0] : nums[1];
+            }
+            int maxof = Max(nums.Skip(1).ToList());
+            return nums[0] > maxof ? nums[0] : maxof;
+        }
+    }
+}
